7f30b96b9603983d93985f30117321f046d12fd3
Olanrewaju <olanrewaju@seamlesshr.com>
2022-11-22 22:34:38 +0100

    add database migration files for orders, products and customers, routes api

---
+++ database/migrations/2022_11_22_091533_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer-id');
+            $table->json('items');
+            $table->float('original_total');
+            $table->float('final_total');
+            $table->float('total_discount')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
